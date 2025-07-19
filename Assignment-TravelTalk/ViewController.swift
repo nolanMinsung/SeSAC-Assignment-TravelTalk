93395770cb8d28038216b5464fe1e04825ddc7f9
Minsung Kim <mskim4048@naver.com>
2025-07-20 01:08:40 +0900

    Initial Commit

---
+++ Assignment-TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment-TravelTalk
+//
+//  Created by 김민성 on 7/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
