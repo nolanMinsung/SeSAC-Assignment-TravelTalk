2538d8396d340d6fe53b7b06216ee2454025c25b
Minsung Kim <mskim4048@naver.com>
2025-07-20 14:34:00 +0900

    feat: 채팅 목록 화면 UI 표시 기능 구현

--- Assignment-TravelTalk/FriendListViewController.swift
+++ Assignment-TravelTalk/FriendListViewController.swift
@@ -9,25 +9,68 @@ import UIKit
 
 class FriendListViewController: UIViewController {
     
+    private(set) var chatRoomList = ChatList.list + ChatList.list
     
     @IBOutlet var serarchBar: UISearchBar!
     
     @IBOutlet var collectionView: UICollectionView!
     
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupCollectionView()
+        setupDelegates()
     }
-    
-    
-    
-    
-    
-    
     
 }
 
+// Initial Settings
+
+extension FriendListViewController {
+    
+    private func setupCollectionView() {
+        // cell registrering
+        let nib = UINib(nibName: "FriendListCell", bundle: .main)
+        collectionView.register(nib, forCellWithReuseIdentifier: "FriendListCell")
+        
+        // setting layout
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        collectionView.collectionViewLayout = flowLayout
+    }
+    
+    private func setupDelegates() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+extension FriendListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        chatRoomList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let chatRoom = chatRoomList[indexPath.item]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FriendListCell", for: indexPath) as! FriendListCell
+        cell.configureData(chatRoom: chatRoom)
+        return cell
+    }
+    
+}
+
+
+// MARK: - UICollectionViewDelegate
+
+extension FriendListViewController: UICollectionViewDelegate {
+    
+}
--- Assignment-TravelTalk/cell/FriendListCell.swift
+++ Assignment-TravelTalk/cell/FriendListCell.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class FriendListCell: UICollectionViewCell {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var chatRoomNameLabel: UILabel!
+    
+    @IBOutlet var latestChatLabel: UILabel!
+    
+    @IBOutlet var latestChatDateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        imageView.layer.cornerRadius = 30
+        imageView.clipsToBounds = true
+        
+        let cellWidth: CGFloat
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            cellWidth = UIScreen.main.bounds.width
+        } else {
+            cellWidth = 320
+        }
+        contentView.widthAnchor.constraint(equalToConstant: cellWidth).isActive = true
     }
-
+    
+    func configureData(chatRoom: ChatRoom) {
+        imageView.image = try? chatRoom.getImage()
+        chatRoomNameLabel.text = chatRoom.chatroomName
+        latestChatLabel.text = chatRoom.latestChat!.message + chatRoom.latestChat!.message
+        latestChatDateLabel.text = chatRoom.latestChat!.dateStringForUI
+    }
+    
 }
