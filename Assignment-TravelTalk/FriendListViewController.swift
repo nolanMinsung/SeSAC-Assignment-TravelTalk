a288ecd79231d5c06f0037280e806feacfca8329
Minsung Kim <mskim4048@naver.com>
2025-07-20 10:51:37 +0900

    add: 트래블톡 기본 뷰컨트롤러 및 outlet 연결

---
+++ Assignment-TravelTalk/FriendListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FriendListViewController.swift
+//  Assignment-TravelTalk
+//
+//  Created by 김민성 on 7/20/25.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+    
+    
+    @IBOutlet var serarchBar: UISearchBar!
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
---
+++ Assignment-TravelTalk/cell/FriendListCell.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListCell.swift
+//  Assignment-TravelTalk
+//
+//  Created by 김민성 on 7/20/25.
+//
+
+import UIKit
+
+class FriendListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
