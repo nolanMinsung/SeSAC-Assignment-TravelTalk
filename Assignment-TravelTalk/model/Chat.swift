f64e4c822921a8cc93b5530a3784eac34db03cbe
Minsung Kim <mskim4048@naver.com>
2025-07-20 14:33:44 +0900

    feat: Chat, ChatRoom 모델 수정

--- Assignment-TravelTalk/model/Chat.swift
+++ Assignment-TravelTalk/model/Chat.swift
@@ -6,9 +6,36 @@
 //
 
 import Foundation
+
+enum ChatError: Error {
+    case invalidDateFormat
+}
+
 //채팅 화면에서 사용할 데이터 구조체
 struct Chat {
     let user: User
     let date: String
+    var dateStringForUI: String {
+        (try? getDateStringForUI()) ?? ""
+    }
     let message: String
+    
+    
+    private func getDate() throws -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        if let date = formatter.date(from: date) {
+            return date
+        } else {
+            throw ChatError.invalidDateFormat
+        }
+    }
+    
+    func getDateStringForUI() throws -> String {
+        guard let date = try? getDate() else { throw ChatError.invalidDateFormat }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy.MM.dd"
+        return formatter.string(from: date)
+    }
+    
 }
--- Assignment-TravelTalk/model/ChatRoom.swift
+++ Assignment-TravelTalk/model/ChatRoom.swift
@@ -5,7 +5,11 @@
 //  Created by Jack on 7/18/25.
 //
 
-import Foundation
+import UIKit
+
+enum ChatRoomError: Error {
+    case roomImageNotFound
+}
 
 //트래블톡 화면에서 사용할 데이터 구조체
 struct ChatRoom {
@@ -13,4 +17,19 @@ struct ChatRoom {
     let chatroomImage: String //채팅방 이미지
     let chatroomName: String //채팅방 이름
     var chatList: [Chat] = [] //채팅 화면에서 사용할 데이터
+    
+    func getImage() throws -> UIImage {
+        let image = UIImage(named: self.chatroomImage)
+        if let image {
+            return image
+        } else {
+            throw ChatRoomError.roomImageNotFound
+        }
+    }
+    
+    /// 가장 마지막(최근) 채팅 정보. 채팅 내역이 없을 경우 `nil` 반환
+    var latestChat: Chat? {
+        chatList.last
+    }
+    
 }
