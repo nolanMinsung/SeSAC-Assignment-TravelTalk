72bba4dc41dc1742f92450419171818e942ffbb7
Minsung Kim <mskim4048@naver.com>
2025-07-23 00:27:05 +0900

    add: ChatModel, ChatRoomModel, UserModel 파일 추가

---
+++ Assignment-TravelTalk/model/Custom Models/ChatModel.swift
@@ -0,0 +1,14 @@
+//
+//  ChatModel.swift
+//  Assignment-TravelTalk
+//
+//  Created by 김민성 on 7/23/25.
+//
+
+import Foundation
+
+
+
+struct ChatModel {
+    
+}
---
+++ Assignment-TravelTalk/model/Custom Models/UserModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserModel.swift
+//  Assignment-TravelTalk
+//
+//  Created by 김민성 on 7/23/25.
+//
+
+import Foundation
+
+
+
+
